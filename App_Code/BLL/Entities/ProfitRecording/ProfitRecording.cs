c85328314858cc4e4d9698c5ebcf085cd56d5d2a
Wimpie Hattingh <wimpie.hattingh@gmail.com>
2017-12-27 09:55:38 +0200

    adjust bitlish fees

--- App_Code/BLL/Entities/ProfitRecording/ProfitRecording.cs
+++ App_Code/BLL/Entities/ProfitRecording/ProfitRecording.cs
@@ -310,7 +310,7 @@ namespace CryptoTrader.BLL
             decimal profPerc = 0;
             try
             {
-                profPerc = GetProfit(bitlishUSD.ask.price, luno.bid.price, usdzar, 0.4M, 3.5M, 2000);
+                profPerc = GetProfit(bitlishUSD.ask.price, luno.bid.price, usdzar, 0.4M, 4.5M, 2000);
 
                 pf = new ProfitRecording();
                 pf.Exchange = enExchange.Bitlish;
@@ -340,7 +340,7 @@ namespace CryptoTrader.BLL
             // Bitlish EUR;
             try
             {
-                profPerc = GetProfit(bitlishEUR.ask.price, luno.bid.price, eurzar, 0.4M, 3.5M, 1500);
+                profPerc = GetProfit(bitlishEUR.ask.price, luno.bid.price, eurzar, 0.4M, 4.5M, 1500);
                 pf = new ProfitRecording();
                 pf.Exchange = enExchange.Bitlish;
                 pf.Currency = enCurrency.EUR;
