e2abd87600bcecccb0637875ff1aa98c1fcfd48f
Wimpie Hattingh <wimpie.hattingh@gmail.com>
2019-06-28 19:17:47 +0200

    update bitlish deposit fees
    d

--- App_Code/BLL/Entities/ProfitRecording/ProfitRecording.cs
+++ App_Code/BLL/Entities/ProfitRecording/ProfitRecording.cs
@@ -315,7 +315,7 @@ namespace CryptoTrader.BLL
             decimal profPerc = 0;
             try
             {
-                profPerc = GetProfit(bitlishUSD.ask.price, luno.bid.price, usdzar, 0.4M, 3.49M, 4000);
+                profPerc = GetProfit(bitlishUSD.ask.price, luno.bid.price, usdzar, 0.4M, 4.84M, 4000);
 
                 pf = new ProfitRecording();
                 pf.Exchange = enExchange.Bitlish;
@@ -345,7 +345,7 @@ namespace CryptoTrader.BLL
             // Bitlish EUR;
             try
             {
-                profPerc = GetProfit(bitlishEUR.ask.price, luno.bid.price, eurzar, 0.4M, 3.49M, 4000);
+                profPerc = GetProfit(bitlishEUR.ask.price, luno.bid.price, eurzar, 0.4M, 4.35M, 4000);
                 pf = new ProfitRecording();
                 pf.Exchange = enExchange.Bitlish;
                 pf.Currency = enCurrency.EUR;
